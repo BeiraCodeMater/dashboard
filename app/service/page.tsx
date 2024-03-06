6b2e2aff1550195c7ed549182f7f18bf94f4c0d3
Arnaldo tomo <arnaldotomo@gmail.com>
2024-03-06 23:03:13 +0200

    Create page.tsx

---
+++ app/service/page.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const service = {
+  getDummy: async () => {
+    const response = await axios.get("https://dummyjson.com/products");
+    return response;
+  },
+  postDummy: async (username: String, password: String) => {
+    const response = await axios.post("https://dummyjson.com/auth/login", {
+      username: username,
+      password: password
+    });
+    return response;
+  }
+};
