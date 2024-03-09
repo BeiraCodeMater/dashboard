81b94cdca24ff3426eb013770d235bc12fd6a7d7
Arnaldo tomo <arnaldotomo@gmail.com>
2024-03-09 13:03:42 +0200

    Update middleware.tsx

--- app/middleware.tsx
+++ app/middleware.tsx
@@ -3,10 +3,24 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+  const token = request.cookies.get("auth_token")?.value;
+
+  const SigInUrl = new URL("/", request.url);
+  const dashboard = new URL("/dashboard", request.url);
+
+  if (!token) {
+    if (request.nextUrl.pathname === "/") {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(SigInUrl);
+  }
+
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(dashboard);
+  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/about/:path*"
+  matcher: ["/", "/dashboard/:path*"]
 };
