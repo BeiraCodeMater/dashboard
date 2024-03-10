6ea8b268228f34b20c14563dabc715cfb2569a95
Arnaldo tomo <arnaldotomo@gmail.com>
2024-03-10 23:51:26 +0200

    Update middleware.tsx

--- app/middleware.tsx
+++ app/middleware.tsx
@@ -3,7 +3,8 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("auth_token")?.value;
+  const token = JSON.parse(localStorage.getItem("auth_token"));
+  // const token = request.cookies.get("auth_token")?.value;
 
   const SigInUrl = new URL("/", request.url);
   const dashboard = new URL("/dashboard", request.url);
